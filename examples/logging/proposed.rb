555bbe65fccf3d5326eb43a2219ab4feb45e3aff
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-08-10 22:56:58 +1200

    Example of proposed improvements to logging.

---
+++ examples/logging/proposed.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'active_job/execution'
+
+module ActiveJob
+  module Execution
+    def perform_now
+      # Guard against jobs that were persisted before we started counting executions by zeroing out nil counters
+      self.executions = (executions || 0) + 1
+
+      deserialize_arguments_if_needed
+
+      _perform_job
+    rescue => error
+      handled = rescue_with_handler(error)
+      return handled if handled
+
+      _default_discard(error)
+    end
+
+    private
+      def _default_discard(error)
+        instrument :discard, error: error do
+          run_after_discard_procs(error)
+        end
+      end
+  end
+end
---
+++ examples/logging/test.rb
@@ -0,0 +1,73 @@
+#!/usr/bin/env ruby
+
+require 'active_job'
+
+if ARGV.include? '--proposed'
+  require_relative 'proposed'
+end
+
+class GoodJob < ActiveJob::Base
+  queue_as :default
+  
+  def perform
+    puts 'Hello, world!'
+  end
+end
+
+class BadJob < ActiveJob::Base
+  queue_as :default
+  
+  def perform
+    raise 'This is a bad job!'
+  end
+end
+
+class RetryJob < ActiveJob::Base
+  queue_as :default
+  retry_on StandardError, wait: 5.seconds, attempts: 3
+  
+  def perform
+    raise 'This is a bad job!'
+  end
+end
+
+class DiscardJob < ActiveJob::Base
+	queue_as :default
+	discard_on StandardError
+	
+	def perform
+		raise 'This is a bad job!'
+	end
+end
+
+def job_server_execute(job_data)
+  ActiveJob::Base.execute(job_data)
+rescue => error
+  # (Q) Should we log this here? Did Rails already log it?
+  $stderr.puts error.full_message
+end
+
+$stdout.puts "### Case 1: Successful Job\n\n```"
+job_data = GoodJob.new.serialize
+job_server_execute(job_data)
+$stdout.puts "```\n\n"
+
+$stdout.puts "### Case 2: Bad Job\n\n```"
+job_data = BadJob.new.serialize
+job_server_execute(job_data)
+$stdout.puts "```\n\n"
+
+$stderr.puts "### Case 3: Error in Job Server\n\n```"
+job_data = {'job_class' => 'DoesNotExistJob'}
+job_server_execute(job_data)
+$stdout.puts "```\n\n"
+
+$stdout.puts "### Case 4: Retry Job\n\n```"
+job_data = RetryJob.new.serialize
+job_server_execute(job_data)
+$stdout.puts "```\n\n"
+
+$stdout.puts "### Case 5: Discard Job\n\n```"
+job_data = DiscardJob.new.serialize
+job_server_execute(job_data)
+$stdout.puts "```\n\n"
